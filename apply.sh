4537fba8f2ccd4c448acac1c4a4aaa96e5aebacf
Burton Copeland <bcope@Burtons-MacBook-Air.local>
2025-10-24 21:58:37 -0500

    Updates to apply.sh script

--- apply.sh
+++ apply.sh
@@ -1,11 +1,16 @@
 cp ./.zshrc    ~/.zshrc
 cp ./.p10k.zsh ~/.p10k.zsh
-cp ./kitty*    ~/.config/kitty/
+cp ./kitty/*   ~/.config/kitty/
 
 pushd ./brew/
 ./brew_install
 popd
 
-echo "Run 'source ~/.zshrc' to finish applying .zshrc changes"
-echo "Type CTRL-CMD-, (Control + command + comma) to apply Kitty changes"
+echo "======================================================================="
+echo "==   Complete!"
+echo "=="
+echo "==   Run 'source ~/.zshrc' to finish applying .zshrc changes"
+echo "=="
+echo "==   Type CTRL-CMD-, (Control + command + comma) to apply Kitty changes (if running kitty)"
+echo "======================================================================="
 
